888c7846852ab58011f90b9c9b8093b54afabdaf
dynamitegus <angus.rose@hotmail.com>
2026-01-12 11:54:06 +1100

    multi layers and multi colours, next: state wide warnings

--- src/app/map-style.ts
+++ src/app/map-style.ts
@@ -1,31 +1,71 @@
 import type { LayerProps } from "react-map-gl/maplibre";
 
 // For more information on data-driven styles, see https://maplibre.org/maplibre-style-spec/expressions/
-export const dataLayer: LayerProps = {
-  id: "data",
+export const unknown: LayerProps = {
+  id: "unknown",
   type: "fill",
-  "filter": [
-    "any",
+  filter: [
+    "all",
     [
       "case",
-      [
-        "==",
-        [
-          "typeof",
-          ["get", "sourceTitle"]
-        ],
-        "string"
-      ],
-      [
-        "==",
-        ["get", "sourceTitle"],
-        "Emergency Warning"
-      ],
+      ["==", ["get", "status"], "Unknown"],
+      ["!=", ["get", "statewide"], "Y"],
       false
-    ]
+    ],
   ],
-  "paint": {
+  paint: {
     "fill-opacity": 0.3,
-    "fill-color": "rgba(255, 0, 0, 1)"
-  }
-}
+    "fill-color": "rgba(0, 255, 0, 1)",
+  },
+};
+export const minor: LayerProps = {
+  id: "minor",
+  type: "fill",
+  filter: [
+    "all",
+    [
+      "case",
+      ["==", ["get", "status"], "Minor"],
+      ["!=", ["get", "statewide"], "Y"],
+      false,
+    ],
+  ],
+  paint: {
+    "fill-opacity": 0.3,
+    "fill-color": "rgba(0, 0, 255, 1)",
+  },
+};
+export const moderate: LayerProps = {
+  id: "moderate",
+  type: "fill",
+  filter: [
+    "all",
+    [
+      "case",
+      ["==", ["get", "status"], "Moderate"],
+      ["!=", ["get", "statewide"], "Y"],
+      false,
+    ],
+  ],
+  paint: {
+    "fill-opacity": 0.3,
+    "fill-color": "#fc6f03",
+  },
+};
+export const extreme: LayerProps = {
+  id: "extreme",
+  type: "fill",
+  filter: [
+    "all",
+    [
+      "case",
+      ["==", ["get", "status"], "Extreme"],
+      ["!=", ["get", "statewide"], "Y"],
+      false,
+    ],
+  ],
+  paint: {
+    "fill-opacity": 0.3,
+    "fill-color": "rgba(255, 0, 0, 1)",
+  },
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,38 @@
-'use client'
-import * as React from 'react';
-import {Map, Source, Layer} from 'react-map-gl/maplibre';
-import {useState, useEffect, useMemo, useCallback} from 'react';
-import 'maplibre-gl/dist/maplibre-gl.css';
-import * as vicEmegencyData from '/geojson.json'
-import {dataLayer} from './map-style';
+"use client";
+import * as React from "react";
+import { Map, Source, Layer } from "react-map-gl/maplibre";
+import { useState, useEffect, useMemo, useCallback } from "react";
+import "maplibre-gl/dist/maplibre-gl.css";
+import * as vicEmegencyData from "/geojson.json";
+import * as dataLayer from "./map-style";
 
 export default function App() {
   const [allData, setAllData] = useState(null);
 
   useEffect(() => {
     /* global fetch */
-    fetch('/geojson.json')
-      .then(resp => resp.json())
-      .then(json => setAllData(json))
-      .catch(err => console.error('Could not load data', err)); // eslint-disable-line
+    fetch("/geojson.json")
+      .then((resp) => resp.json())
+      .then((json) => setAllData(json))
+      .catch((err) => console.error("Could not load data", err)); // eslint-disable-line
   }, []);
   return (
     <Map
       initialViewState={{
         longitude: 144.9464,
         latitude: -37.8409,
-        zoom: 6
+        zoom: 6,
       }}
-      interactiveLayerIds={['data']}
-      style={{width: 1200, height: 500}}
+      interactiveLayerIds={["data"]}
+      style={{ width: 1200, height: 500 }}
       mapStyle="https://tiles.versatiles.org/assets/styles/colorful/style.json"
-      
     >
-        <Source type="geojson" data={allData}>
-          <Layer {...dataLayer} />
-        </Source>
+      <Source type="geojson" data={allData}>
+        <Layer {...dataLayer.minor} />
+        <Layer {...dataLayer.moderate} />
+        <Layer {...dataLayer.extreme} />
+        <Layer {...dataLayer.unknown} />
+      </Source>
     </Map>
   );
 }
